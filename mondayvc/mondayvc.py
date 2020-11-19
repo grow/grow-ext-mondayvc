564b67e1f77e56c8d34ca849a932eed681d2f76b
Mike King <kingmi@google.com>
2020-11-19 09:30:42 -1000

    b/173643145 - Google Gradient - Jobs - Change organizations -> companies and collections -> networks

--- mondayvc/mondayvc.py
+++ mondayvc/mondayvc.py
@@ -21,47 +21,47 @@ class AttributeMessage(messages.Message):
 
 class MondayVCPreprocessor(grow.Preprocessor):
     KIND = 'mondayvc'
-    JOBS_URL = 'https://api.getro.com/v2/networks/{collections_id}/jobs'
-    COLLECTIONS_URL = 'https://api.getro.com/v2/networks'
-    ORGANIZATIONS_URL = 'https://api.getro.com/v2/networks/{collections_id}/organizations'
+    JOBS_URL = 'https://api.getro.com/v2/networks/{networks_id}/jobs'
+    NETWORKS_URL = 'https://api.getro.com/v2/networks'
+    ORGANIZATIONS_URL = 'https://api.getro.com/v2/networks/{networks_id}/companies'
     QUERY_PARAM = '?page={number_of_page}&per_page=100'
-    COLLECTIONS_ID = ''
+    NETWORKS_ID = ''
 
     class Config(messages.Message):
         api_user = messages.StringField(1)
         api_key = messages.StringField(2)
         jobs_path = messages.StringField(4)
-        collections_path = messages.StringField(5)
-        organizations_path = messages.StringField(6)
+        networks_path = messages.StringField(5)
+        companies_path = messages.StringField(6)
 
-    def bind_collections(self, collections_path):
+    def bind_networks(self, networks_path):
         headers = { 'X-User-Email': self.config.api_user, 'X-User-Token': self.config.api_key, 'Content-Type': 'application/json', 'Accept': 'application/json'}
-        url = MondayVCPreprocessor.COLLECTIONS_URL
+        url = MondayVCPreprocessor.NETWORKS_URL
         resp = requests.get(url, headers=headers)
         if resp.status_code != 200:
                 raise Error('Error requesting -> {}'.format(url))
-        collections = resp.json()
-        MondayVCPreprocessor.COLLECTIONS_ID = collections['items'][0]['id']
-        path = os.path.join(collections_path)
-        self.pod.write_yaml(path, collections)
+        networks = resp.json()
+        MondayVCPreprocessor.NETWORKS_ID = networks['items'][0]['id']
+        path = os.path.join(networks_path)
+        self.pod.write_yaml(path, networks)
         self.pod.logger.info('Saving -> {}'.format(path))
 
-    def bind_organizations(self, collections_id, organizations_path):
+    def bind_companies(self, networks_id, companies_path):
         headers = { 'X-User-Email': self.config.api_user, 'X-User-Token': self.config.api_key, 'Content-Type': 'application/json', 'Accept': 'application/json'}
-        url = MondayVCPreprocessor.ORGANIZATIONS_URL.format(collections_id=collections_id) + MondayVCPreprocessor.QUERY_PARAM.format(number_of_page=1)
+        url = MondayVCPreprocessor.ORGANIZATIONS_URL.format(networks_id=networks_id) + MondayVCPreprocessor.QUERY_PARAM.format(number_of_page=1)
         resp = requests.get(url, headers=headers)
         if resp.status_code != 200:
                 raise Error('Error requesting -> {}'.format(url))
-        organizations = resp.json()
-        path = os.path.join(organizations_path)
-        self.pod.write_yaml(path, organizations)
+        companies = resp.json()
+        path = os.path.join(companies_path)
+        self.pod.write_yaml(path, companies)
         self.pod.logger.info('Saving -> {}'.format(path))
 
-    def bind_jobs(self, collections_id, jobs_path):
+    def bind_jobs(self, networks_id, jobs_path):
         jobs = {}
         headers = { 'X-User-Email': self.config.api_user, 'X-User-Token': self.config.api_key, 'Content-Type': 'application/json', 'Accept': 'application/json'}
         def get_jobs(number_of_page=1):
-            url = MondayVCPreprocessor.JOBS_URL.format(collections_id=collections_id) + MondayVCPreprocessor.QUERY_PARAM.format(number_of_page=number_of_page)
+            url = MondayVCPreprocessor.JOBS_URL.format(networks_id=networks_id) + MondayVCPreprocessor.QUERY_PARAM.format(number_of_page=number_of_page)
             resp = requests.get(url, headers=headers)
             if resp.status_code != 200:
                 raise Error('Error requesting -> {}'.format(url))
@@ -80,14 +80,14 @@ class MondayVCPreprocessor(grow.Preprocessor):
         self.pod.logger.info('Saving -> {}'.format(path))
 
     def run(self, *args, **kwargs):
-        if self.config.collections_path:
-            self.bind_collections(
-                self.config.collections_path)
-        if self.config.organizations_path:
-            self.bind_organizations(
-                MondayVCPreprocessor.COLLECTIONS_ID,
-                self.config.organizations_path)
+        if self.config.networks_path:
+            self.bind_networks(
+                self.config.networks_path)
+        if self.config.companies_path:
+            self.bind_companies(
+                MondayVCPreprocessor.NETWORKS_ID,
+                self.config.companies_path)
         if self.config.jobs_path:
             self.bind_jobs(
-                MondayVCPreprocessor.COLLECTIONS_ID,
+                MondayVCPreprocessor.NETWORKS_ID,
                 self.config.jobs_path)
